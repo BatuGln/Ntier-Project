2b84353db6463245e315f342b7b6b93f9d8fa7c1
Batu <Batu@LAPTOP-052K89PA>
2024-06-29 10:35:04 +0300

    Add project files.

---
+++ Ntier.UI/Controllers/RoleController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Ntier.Data.Models.Identity;
+
+namespace Ntier.UI.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            if (_roleManager.Roles.ToList() == null)
+                return NotFound("Roles is not found!!!");
+            return View(_roleManager.Roles.Where(x => x.Name != "Admin").ToList());
+        }
+    }
+}
